90eaf72d57d0237a1e98887cbf09144f8f9d252e
anon-ml-repo <julballa@outlook.com>
2025-08-30 00:33:02 +0200

    initial commit

--- main.sh
+++ main.sh
@@ -1,23 +1,21 @@
-# Implicit Augmentation from Distributional Symmetry in Turbulence Super-Resolution
-# Requirments
-To install requirements:
-```
-pip install -r requirements.
-```
-# To get Dataset:
-```
+#!/bin/bash
+set -e  # stop on error
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Download dataset
+echo "Installing dataset"
 curl -L -o numpy_middle.zip "https://zenodo.org/record/16997999/files/numpy_middle.zip?download=1"
 unzip -o numpy_middle.zip -d dataset 
 curl -L -o numpy_nearwall.zip "https://zenodo.org/record/16997999/files/numpy_nearwall.zip?download=1"
-unzip -o numpy_near_wall.zip -d dataset
-```
-# To run the model and get the plots:
-```
+unzip -o numpy_nearwall.zip -d dataset
+
+
+# Run training
+echo "Running training script on middle bos"
 python3 src/train.py configs/middle_boxfilter_4x_MultiScale.yaml
+
+echo "Running training script on nearwall box"
 python3 src/train.py configs/nearwall_boxfilter_4x_MultiScale.yaml
-```
-# End-to-End pipeline:
-```
-./main.sh
-```
 
---
+++ src/global_config.py
@@ -0,0 +1,4 @@
+# numpy_dir = '/data/NFS/potato/turbulence/numpy'
+numpy_dir = "dataset"
+# experiment_outputs = '/data/NFS/potato/rmcconke/implicit_data_augmentation/experiment_outputs'
+experiment_outputs = "./outputs"
